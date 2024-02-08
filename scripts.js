f610504be3f0c61bef61a5026bdc9dcdff86f91f
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-07 22:35:22 -0300

    add new list scheme

--- scripts.js
+++ scripts.js
@@ -1,8 +1,28 @@
 const button = document.querySelector('.button-add-task');
 const input = document.querySelector('.input-task');
 
+let tasks = [];
+
+
+
 function addTask() {
-    console.log(input.value);
+    tasks.push(input.value);
+    renderTasks();
   }
+
+function renderTasks(){
+
+    let newList =
+    addTask.forEach(task => {
+        newList = newList +`
+        <li class="task">
+                <img src="./img/checked.png" alt="Check">
+                <p>${task}</p>
+                <img src="./img/trash.png" alt="Lixeira">
+            </li>
+            `
+})
+  
+}
 
 button.addEventListener('click', addTask);
