2de2d05dd686b6e425af130fe9c96a9fda0db3ca
Carlos Vinícius Pereira <p.carlosvini@gmail.com>
2024-02-07 23:08:50 -0300

    Add list function for add items

--- scripts.js
+++ scripts.js
@@ -1,28 +1,27 @@
 const button = document.querySelector('.button-add-task');
 const input = document.querySelector('.input-task');
+const fullList = document.querySelector('.list-tasks');
 
 let tasks = [];
-
-
 
 function addTask() {
     tasks.push(input.value);
     renderTasks();
-  }
+    input.value = "";
+}
 
-function renderTasks(){
-
-    let newList =
-    addTask.forEach(task => {
-        newList = newList +`
+function renderTasks() {
+    let newList = "";
+    tasks.forEach(task => {
+        newList = newList + `
         <li class="task">
                 <img src="./img/checked.png" alt="Check">
                 <p>${task}</p>
                 <img src="./img/trash.png" alt="Lixeira">
             </li>
-            `
-})
-  
+            `;
+    });
+    fullList.innerHTML = newList;
 }
 
 button.addEventListener('click', addTask);
